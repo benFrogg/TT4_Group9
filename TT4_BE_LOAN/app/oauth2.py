dfe0915fd22ce763dcd700d88c0670314f202c4a
ShawnGoh <goh_shawn1998@hotmail.com>
2022-03-25 15:40:14 +0800

    updated secret key to alvins and updated verify access token to get the required output

--- TT4_BE_LOAN/app/oauth2.py
+++ TT4_BE_LOAN/app/oauth2.py
@@ -28,9 +28,9 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY)
-
-        id:str = payload.get("user_id")
-        role:str = payload.get("role")
+        token = payload.get("jwtpayload")
+        id:str = token.get("customer_id")
+        role:str = token.get("role")
         if id is None:
             raise credentials_exception
         if role is None:
