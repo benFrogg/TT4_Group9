c39e7ea419be0259cabc60c14975aedf1083f8fd
benfrog <angqishao@gmail.com>
2022-03-25 16:09:47 +0800

    NewLoan done

--- TT4_FE/src/components/NewLoan/NewLoan.js
+++ TT4_FE/src/components/NewLoan/NewLoan.js
@@ -1,13 +1,17 @@
 import './NewLoan.css';
 
-import React from 'react'
+import React, { useRef } from 'react'
 
 function NewLoan() {
+    const inputFile = useRef(null)
+
   return (
     <div>
+        <h2>Apply New Loan</h2>
         <form>
             <label>Full Name</label>
-            <input
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+
         </form>
     </div>
   )
--- TT4_FE/src/components/StartingPage/StartingPageContent.js
+++ TT4_FE/src/components/StartingPage/StartingPageContent.js
@@ -1,10 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import classes from './StartingPageContent.module.css';
 import AuthContext from '../../store/auth-context';
 import axios from "axios";
 
 const StartingPageContent = () => {
   const loggedIn = useContext(AuthContext)
+  const CPFHist = useRef(null)
+  const incomeTaxFile = useRef(null)
+  const [ newLoanAmount, setNewLoan ] = useState("");
+
+  const onClickCPF = (e) => {
+    CPFHist.current.click();
+  }
+
+  const onClickTax = (e) => {
+    incomeTaxFile.current.click();
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newLoanAmount)
+  }
 
   return (
     <section className={classes.starting}>
@@ -31,21 +47,28 @@ const StartingPageContent = () => {
                 </tbody>
               </table>
               <div>
-                <h2>Apply new loan</h2>
-                <table  style={{border: "2px solid green", alignItems: "center"}}>
-                  <thead>
-                    <tr>
-                      <th>NRIC</th>
-                      <th>CPF contribution statement</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                  </tbody>
-                </table>
+                  <h2>Apply New Loan</h2>
+                  <form onSubmit={handleSubmit}>
+                      <label>Full Name: </label>
+                      <br/>
+                      <input type="text" />
+                      <br/>
+                      <label>NRIC: </label>
+                      <br/>
+                      <input type="text" />
+                      <br/>
+                      <label>CPF Contribution History Statement (Latest 9 months)</label><br/>
+                      <input type="file" id="file" ref={CPFHist} style={{display: 'none'}} />
+                      <button onClick={onClickCPF}>Open file browser</button><br/>
+                      <label>Latest 1 Year Income Tax Notice of Assessment</label>
+                      <input type="file" id="file" ref={incomeTaxFile} style={{display: 'none'}} /><br/>
+                      <button onClick={onClickTax}>Open file browser</button>
+                      <br/>
+                      <label>Loan Amount</label><br/>
+                      <input type="text" value={newLoanAmount} onChange={(e) => setNewLoan(e.target.value)}/>
+                      <br/>
+                      <button type="submit">Submit</button>
+                  </form>
               </div>
               <div>
                 <h2>Pay back outstanding loans</h2>
